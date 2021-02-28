99566d8863067120e19d3fdde8128f7f7b5d136c
John-Rotte <john.roth_95@live.com>
2021-02-28 11:33:36 +0100

    Added basic update and replace

--- src/main/java/se/iths/springproject/services/SongService.java
+++ src/main/java/se/iths/springproject/services/SongService.java
@@ -1,7 +1,10 @@
 package se.iths.springproject.services;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import se.iths.springproject.dtos.SongDto;
+import se.iths.springproject.dtos.SongTitle;
 import se.iths.springproject.entities.Song;
 import se.iths.springproject.mappers.SongMapper;
 import se.iths.springproject.repositories.SongRepository;
@@ -40,5 +43,49 @@ public class SongService {
         return songMapper.mapp(songRepository.save(songMapper.mapp(songDto)));
     }
 
+    public void delete(int id) {
+        if (songRepository.findById(id).isEmpty())
+            throw new RuntimeException();
+        songRepository.deleteById(id);
+    }
+
+    public SongDto replace(int id, SongDto songDto) {
+        Optional <Song> song = songRepository.findById(id);
+        if (song.isPresent())
+        {
+            Song updatedSong = song.get();
+            if (songDto.getTitle() != null)
+            updatedSong.setTitle(songDto.getTitle());
+            if (songDto.getAlbum() != null)
+            updatedSong.setAlbum(songDto.getAlbum());
+            if (songDto.getArtist() != null)
+                updatedSong.setArtist(songDto.getArtist());
+            if (songDto.getLength() != 0)
+                updatedSong.setLength(songDto.getLength());
+
+            return songMapper.mapp(songRepository.save(updatedSong));
+        }
+        else
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                "Song with id " + id + " not found");
+
+    }
+
+    public SongDto update(int id, SongTitle songTitle) {
+        Optional <Song> song = songRepository.findById(id);
+        if (song.isPresent())
+        {
+            Song updatedSong = song.get();
+            if (songTitle.getTitle() != null)
+                updatedSong.setTitle(songTitle.getTitle());
+
+
+            return songMapper.mapp(songRepository.save(updatedSong));
+        }
+        else
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Song with id " + id + " not found");
+
+    }
 
 }
